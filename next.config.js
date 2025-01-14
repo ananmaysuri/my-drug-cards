ab0052c609dc39d1995dd9c2867b0fe72516a0f1
ananmay-suri <ananmaysuri2000@gmail.com>
2025-01-13 22:23:40 -0500

    Error free out

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'export',  // Add this for static export
+  basePath: '/drug-cards', // Add this - should match your repository name
+  images: {
+    unoptimized: true // Add this for static export
+  }
 }
 
 module.exports = nextConfig
